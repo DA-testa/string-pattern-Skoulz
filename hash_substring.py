1916a32f67648c3554b4fbf40bbcc60d7344a822
Artjoms Sidorkins <117168579+Skoulz@users.noreply.github.com>
2023-04-16 22:21:59 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,9 +1,8 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
+    # this function needs to acquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
 
     users_input = input()
 
@@ -13,12 +12,10 @@ def read_input():
         return (pattern, text)
 
     elif "F" in users_input:
-        with open(str("./tests/06"), mode="r", encoding='UTF-8') as files:
-            pattern = files.readline().rstrip()
-            text = files.readline().rstrip()
+        with open(str("./tests/06"), mode="r", encoding='UTF-8') as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
         return (pattern, text)
-    
-
     # after input type choice
     # read two lines 
     # first line is pattern 
@@ -29,29 +26,23 @@ def read_input():
 
     # this is the sample return, notice the rstrip function
    # return (pattern, text)
-
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    # this function should find the occurrences using Rabin Karp algorithm
     pattern_length, text_length = len(pattern), len(text)
-    users_hash_1 = hash(pattern)
-    users_hash_2 = hash(text[:pattern_length])
+    pattern_hash = hash(pattern)
+    text_hash = hash(text[:pattern_length])
     occurrences = []
     for i in range(text_length - pattern_length + 1):
-        if users_hash_1 == users_hash_2 and pattern == text[i:i+pattern_length]:
+        if pattern_hash == text_hash and pattern == text[i:i+pattern_length]:
             occurrences.append(i)
         if i < text_length - pattern_length:
-            users_hash_2 = (users_hash_2 - ord(text[i]) * pow(10, pattern_length-1)) * 10 + ord(text[i+pattern_length])
+            text_hash = (text_hash - ord(text[i]) * pow(10, pattern_length-1)) * 10 + ord(text[i+pattern_length])
     return occurrences
-    
-    # and return an iterable variable
-   #return [0]
-
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
